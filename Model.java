06ab7cf78a51643e3b562c08e0db7ba9d9e1dbdd
MarcoMatouschek <marcomatouschek@gmail.com>
2016-11-25 18:35:57 +0100

    Update Model.java

--- Model.java
+++ Model.java
@@ -1,1 +1,34 @@
-
+public class Model{
+  
+  private static final int EASY;
+  private static final int MED;
+  private static final int HARD;
+ 
+  private boolean[][] value;
+  
+  public Model(){
+    EASY = 25;
+    MED = 15;
+    HARD = 10;
+    init(2);
+  }
+  
+  public boolean[][] getValue(){
+    return this.value;
+  }
+  
+  public void init(int diff){
+    int size;
+    if(diff == 1) size = EASY;
+    else if(diff == 2) size = MED;
+    else if(diff == 3) size = HARD;
+    
+    this.value = new boolean[size][size];
+    
+    for(int i=0; i<size; i++){
+      for(int j=0; j<size; j++){
+        this.value[i][j] = randomBoolean();
+      }
+    }
+  }
+}
